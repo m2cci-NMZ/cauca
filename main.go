c4df4071c84aa0ed5a492bdad62e3a41ee5f623a
m2cci-NMZ <nico100885@hotmail.Com>
2022-02-01 18:06:51 +0100

    changed path file

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	var memory Memory
 	var cpu Register
-	memory.loadRom("test")
+	memory.loadRom("roms/tetris")
 	var i uint16 = 0
 	for i < 1000 {
 		cpu.execute(memory.readByte(cpu.pc), &memory)
