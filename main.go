368aac376e259c2f1fb756188aad028cb6ad6909
m2cci-NMZ <nico100885@hotmail.Com>
2022-02-22 11:34:48 +0100

    removed vram viewer window

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	var display Display
 	memory.loadRom("roms/tetris")
 	defer display.close()
-	defer display.vramClose()
+	//defer display.vramClose()
 	//var i int = 0
 	cpu.a = 0x01
 	cpu.flags = 0xb0
@@ -22,7 +22,7 @@ func main() {
 	cpu.sp = 0xfffe
 	cpu.pc = 0x100
 	display.init()
-	display.initVramViewer()
+	//display.initVramViewer()
 	//memory.writeByte(0xff44, 0x94)
 	for display.running {
 		//for cpu.pc != 0x2a0 {
@@ -33,7 +33,7 @@ func main() {
 		cpu.execute(memory.readByte(cpu.pc), &memory)
 		gpu.step(cpu, &memory)
 		display.display(gpu)
-		display.displayVram(gpu, memory)
+		//display.displayVram(gpu, memory)
 		//i++
 	}
 	//os.WriteFile("tile.bin", memory.vram[:], 0777)
