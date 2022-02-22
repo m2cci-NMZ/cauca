dd77e5f115ed4830a2516d0240801c4a925d4225
m2cci-NMZ <nico100885@hotmail.Com>
2022-02-22 11:07:39 +0100

    Got display kinda working FINALLY

--- display.go
+++ display.go
@@ -15,7 +15,7 @@ type Display struct {
 	running      bool
 }
 
-const tile_size int = 8
+const tile_size int = 2
 
 func (display *Display) init() int {
 	var err error
@@ -37,7 +37,7 @@ func (display *Display) init() int {
 func (display *Display) initVramViewer() int {
 	var err error
 	display.vramWindow, err = sdl.CreateWindow("GB", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		8*int32(tile_size), 26*int32(tile_size), sdl.WINDOW_SHOWN)
+		8*int32(tile_size), int32(numtiles)*8*int32(tile_size), sdl.WINDOW_SHOWN)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to create window: %s\n", err)
 		return 1
@@ -62,12 +62,12 @@ func (display *Display) display(gpu Gpu) int {
 		}
 	}
 	if gpu.rendering {
-		display.renderer.SetDrawColor(0, 0, 0, 0)
+		display.renderer.SetDrawColor(255, 255, 255, 0)
 		display.renderer.Clear()
 		for x := 1; x < 160; x++ {
 			for y := 1; y < 144; y++ {
 				if gpu.frame_buffer[x][y] > 0 {
-					display.renderer.SetDrawColor(255, 255, 255, 255)
+					display.renderer.SetDrawColor(0, 0, 0, 255)
 					display.renderer.DrawPoint(int32(x), int32(y))
 				}
 			}
@@ -82,12 +82,12 @@ func (display *Display) displayVram(gpu Gpu, mem Memory) int {
 	display.running = true
 	vram := gpu.getVram(mem)
 	if gpu.rendering {
-		display.vramRenderer.SetDrawColor(0, 0, 0, 255)
+		display.vramRenderer.SetDrawColor(255, 255, 255, 0)
 		display.vramRenderer.Clear()
 		for x := 0; x < 8*tile_size; x++ {
-			for y := 0; y < 26*tile_size; y++ {
+			for y := 0; y < numtiles*8*tile_size; y++ {
 				if vram[y/tile_size][x/tile_size] > 0 {
-					display.vramRenderer.SetDrawColor(255/2, 255/2, 255/2, 255)
+					display.vramRenderer.SetDrawColor(0, 0, 0, 255)
 					display.vramRenderer.DrawPoint(int32(x), int32(y))
 				}
 			}
